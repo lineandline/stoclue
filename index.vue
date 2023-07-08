314a236c8183cd500041e9a37e993524e490eab2
lineandline <108500585+lineandline@users.noreply.github.com>
2023-07-08 17:41:11 +0800

    Add files via upload

---
+++ index.vue
@@ -0,0 +1,148 @@
+<template>
+	<view>
+		<!-- <u-navbar :is-back="false" title="　" :border-bottom="false">
+			<view class="u-flex u-row-right" style="width: 100%;">
+				<view class="camera u-flex u-row-center">
+					<u-icon name="camera-fill" color="#000000" size="48"></u-icon>
+				</view>
+			</view>
+		</u-navbar> -->
+		<!-- u-p-l-30   padding-lefe -->
+		<!-- 登录后  v-if: vue 语法  判断是否成立    if-else -->
+		<view v-if="isLogin">
+			<view class="u-flex user-box u-p-l-30 u-p-r-20 u-p-b-30">
+				<view class="u-m-r-10">
+					<!-- 动态属性 从data获取-->
+					<u-avatar :src="pic" size="140"></u-avatar>
+				</view>
+				<view class="u-flex-1">
+					<view class="u-font-18 u-p-b-20">uView ui</view>
+					<view class="u-font-14 u-tips-color">部门:helang_uView</view>
+				</view>
+				<view class="u-m-l-10 u-p-10">
+					<u-icon name="scan" color="#969799" size="28"></u-icon>
+				</view>
+				<view class="u-m-l-10 u-p-10">
+					<u-icon name="arrow-right" color="#969799" size="28"></u-icon>
+				</view>
+			</view>
+			
+			<view class="u-m-t-20">
+				<u-cell-group>
+					<u-cell-item icon="rmb-circle" title="支付"></u-cell-item>
+				</u-cell-group>
+			</view>
+			
+			<view class="u-m-t-20">
+				<u-cell-group>
+					<u-cell-item icon="star" title="收藏"></u-cell-item>
+					<u-cell-item icon="photo" title="相册"></u-cell-item>
+					<u-cell-item icon="coupon" title="卡券"></u-cell-item>
+					<u-cell-item icon="heart" title="关注"></u-cell-item>
+				</u-cell-group>
+			</view>
+			
+			<view class="u-m-t-20">
+				<u-cell-group>
+					<u-cell-item icon="setting" title="设置"></u-cell-item>
+				</u-cell-group>
+			</view>
+		</view>
+		<!-- 未登录 -->
+		<view v-else>
+			<view class="u-flex user-box u-p-l-30 u-p-r-20 u-p-b-30">
+				<view class="u-m-r-10">
+					<!-- 动态属性 从data获取-->
+					<u-avatar :src="pic" size="140"></u-avatar>
+				</view>
+				<view class="u-flex-1">
+					<view class="u-font-18 u-p-b-20">uView ui</view>
+					<view class="u-font-14 u-tips-color">部门:zhangsan_uView</view>
+				</view>
+				<view class="u-m-l-10 u-p-10">
+					<u-icon name="scan" color="#969799" size="28"></u-icon>
+				</view>
+				<view class="u-m-l-10 u-p-10">
+					<u-icon name="arrow-right" color="#969799" size="28"></u-icon>
+				</view>
+			</view>
+			<view class="u-m-t-20">
+				<!-- 事件处理  @开头   click表示点击后触发的函数 -->
+				<u-button  @click="doLogin">登录</u-button>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				pic:'https://uviewui.com/common/logo.png',
+								isLogin:false
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			doLogin:function(e){ //e：事件对象
+						//js函数语句:  一句话占一行,;可省略   
+						//console.log()打印信息到控制台
+							console.log(e);
+							console.log("触发了点击事件，跳转到登录页");
+							uni.navigateTo({
+								url:'/pages/login/login'
+							})
+						}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 40rpx;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 100rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+    .imaget {
+		height: 800rpx;
+		width:800rpx;
+		margin-top: auto;
+		margin-left:auto;
+		margin-right: auto;
+		margin-bottom: auto;
+	}
+
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+	
+	.title {
+		font-size: 28rpx;
+		color: $u-content-color;
+	}
+	
+	.button-demo {
+		margin-top: 80rpx;
+	}
+	
+	.link-demo {
+		margin-top: 80rpx;
+	}
+</style>
